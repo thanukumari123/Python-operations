cfb708a171516df78ea6ff4a3c742c10b8eadebd
thanukumari123 <kumarithanu324@gmail.com>
2026-05-04 19:46:00 +0530

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,14 @@
+a = 5   
+b = 3   
+# AND
+print("AND (&):", a & b)
+# OR
+print("OR (|):", a | b)
+# XOR
+print("XOR (^):", a ^ b)
+# NOT
+print("NOT (~a):", ~a)
+# Left Shift
+print("Left Shift (a << 1):", a << 1)
+# Right Shift
+print("Right Shift (a >> 1):", a >> 1)
