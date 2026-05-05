10360d81d78854941e956ae7970dc11ceaf312bd
thanukumari123 <kumarithanu324@gmail.com>
2026-05-05 11:56:20 +0530

    Add files via upload

---
+++ relational.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+# Equal to
+print("a == b:", a == b)
+# Not equal to
+print("a != b:", a != b)
+# Greater than
+print("a > b:", a > b)
+# Less than
+print("a < b:", a < b)
+# Greater than or equal to
+print("a >= b:", a >= b)
+# Less than or equal to
+print("a <= b:", a <= b)
